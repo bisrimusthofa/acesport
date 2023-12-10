3422a7d99e564599a86b0f6c9c0a512e80ae71fc
Bisri Musthofa <bisrimustafa220@gmail.com>
2023-12-10 23:53:20 +0700

    compose yml

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	dsn := "root:@tcp(localhost:3306)/acesport?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:@tcp(mysql_container:3306)/acesport?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	helper.PanicIfError(err)
 
